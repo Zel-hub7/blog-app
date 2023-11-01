71f0d3d49df24cf46399c4653c52d07f8bc6f728
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-01 14:22:33 +0300

    add foreign key to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_01_111552) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_01_111914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,4 +57,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_01_111552) do
     t.integer "posts_counter"
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
