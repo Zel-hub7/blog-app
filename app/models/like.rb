08d76e61b921775937e017ab863e0ea61ced14d0
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-02 11:53:14 +0300

    update files

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
     belongs_to :user, foreign_key: 'user_id' class_name: 'User'
     belongs_to :post, foreign_key: 'post_id' class_name: 'Post'
+
+    after_create :update_post_likes_counter
+
+    private
+  
+    def update_post_likes_counter
+      post.increment!(:likes_counter)
+    end
 end
