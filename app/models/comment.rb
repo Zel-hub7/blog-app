2be74ca7d0dd132ce70092477c49a697c0eaa519
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-02 12:01:08 +0300

    fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :user, foreign_key: 'user_id', class_name: 'User'
-    belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
+  belongs_to :user, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
 
-    after_create :update_post_comments_counter
+  after_create :update_post_comments_counter
 
-    private
-  
-    def update_post_comments_counter
-      post.increment!(:comments_counter)
-    end
+  private
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :user, foreign_key: 'author_id', class_name: 'User'
-    has_many :comments, foreign_key: 'post_id'
-    has_many :likes, foreign_key: 'post_id'
+  belongs_to :user, foreign_key: 'author_id', class_name: 'User'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 
-    after_create :update_user_posts_counter
+  after_create :update_user_posts_counter
 
-    private
-  
-    def update_user_posts_counter
-      user.increment!(:posts_counter)
-    end
-    
+  private
+
+  def update_user_posts_counter
+    user.increment!(:posts_counter)
+  end
 end
