928d5e41f2c5e54f414666b608433511c364c50c
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-11-02 13:27:03 +0300

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,11 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post
 
   after_create :update_post_likes_counter
-
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
+  
   private
 
   def update_post_likes_counter
