a79472d2ba7d5c81f8c2a818e0286f7ac7399bb6
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-02 12:38:01 +0300

    made the neccessary changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,16 @@
 class Like < ApplicationRecord
-    belongs_to :user, foreign_key: 'user_id' class_name: 'User'
-    belongs_to :post, foreign_key: 'post_id' class_name: 'Post'
+  belongs_to :user
+  belongs_to :post
 
-    after_create :update_post_likes_counter
+  after_create :update_post_likes_counter
 
-    private
-  
-    def update_post_likes_counter
-      post.increment!(:likes_counter)
-    end
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
 
   after_create :update_user_posts_counter
 
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+
   private
 
   def update_user_posts_counter
