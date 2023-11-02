8a7fb03045b08bba8dc4f04b8746223b51718721
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-02 11:50:55 +0300

    add features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :user, foreign_key: 'author_id', class_name: 'User'
+    has_many :comments, foreign_key: 'post_id'
+    has_many :likes, foreign_key: 'post_id'
 end
