b19e9fcb6a0b40b6c82c554148a12cb60040e0de
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-07 14:01:14 +0300

    add files and fix linter errors

--- spec/requests/posts_controllers_spec.rb
+++ spec/requests/posts_controllers_spec.rb
@@ -13,6 +13,26 @@ RSpec.describe PostsController, type: :request do
       get user_posts_path(745) # Replace with the actual user ID
       expect(response).to render_template('index')
     end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_posts_path(745) # Replace with the actual user ID
+      expect(response.body).to include('User name')
+      expect(response.body).to include('Number of posts: x')
+      expect(response.body).to include('Post #1')
+      expect(response.body).to include('Lorem ....')
+      expect(response.body).to include('Username: Comment 1')
+      expect(response.body).to include('Username: Comment 2')
+      expect(response.body).to include('Username: Comment 3')
+      expect(response.body).to include('Username: Comment 4')
+      expect(response.body).to include('Username: Comment 5')
+      expect(response.body).to include('Post #2')
+      expect(response.body).to include('Lorem ipsum....')
+      expect(response.body).to include('Username: Comment 1')
+      expect(response.body).to include('Username: Comment 2')
+      expect(response.body).to include('Username: Comment 3')
+      expect(response.body).to include('Username: Comment 4')
+      expect(response.body).to include('Username: Comment 5')
+    end
   end
 
   describe 'GET #show' do
@@ -24,6 +44,17 @@ RSpec.describe PostsController, type: :request do
     it "renders the 'show' template" do
       get user_post_path(745, 3) # Replace with the actual user and post IDs
       expect(response).to render_template('show')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_post_path(745, 3) # Replace with the actual user and post IDs
+      expect(response.body).to include('Post #1 by username')
+      expect(response.body).to include('Lorem ....')
+      expect(response.body).to include('Username: Comment 1')
+      expect(response.body).to include('Username: Comment 2')
+      expect(response.body).to include('Username: Comment 3')
+      expect(response.body).to include('Username: Comment 4')
+      expect(response.body).to include('Username: Comment 5')
     end
   end
 end
--- spec/requests/users_controllers_spec.rb
+++ spec/requests/users_controllers_spec.rb
@@ -13,17 +13,37 @@ RSpec.describe UsersController, type: :request do
       get users_path
       expect(response).to render_template('index')
     end
+
+    it 'includes correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('User Name')
+      expect(response.body).to include('Number of posts x')
+    end
   end
 
   describe 'GET #show' do
     it 'returns a successful response' do
-      get user_path(745) # Replace with the actual user ID
+      get user_path(745)
       expect(response).to have_http_status(:success)
     end
 
     it "renders the 'show' template" do
-      get user_path(745) # Replace with the actual user ID
+      get user_path(745)
       expect(response).to render_template('show')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_path(745)
+      expect(response.body).to include('User name')
+      expect(response.body).to include('Number of posts: x')
+      expect(response.body).to include('Bio')
+      expect(response.body).to include('Lorem..............')
+      expect(response.body).to include('Post #1')
+      expect(response.body).to include('Lorem...................')
+      expect(response.body).to include('Post #2')
+      expect(response.body).to include('Lorem...................')
+      expect(response.body).to include('Post #3')
+      expect(response.body).to include('Lorem...................')
     end
   end
 end
