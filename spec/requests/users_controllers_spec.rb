80760b20f866978908c061d40141cbeb7f24d3b2
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-07 11:31:37 +0300

    fix linter errors

--- spec/requests/users_controllers_spec.rb
+++ spec/requests/users_controllers_spec.rb
@@ -3,27 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
-  describe "GET #index" do
-    it "returns a successful response" do
+  describe 'GET #index' do
+    it 'returns a successful response' do
       get users_path
       expect(response).to have_http_status(:success)
     end
 
     it "renders the 'index' template" do
       get users_path
-      expect(response).to render_template("index")
+      expect(response).to render_template('index')
     end
   end
 
-  describe "GET #show" do
-    it "returns a successful response" do
+  describe 'GET #show' do
+    it 'returns a successful response' do
       get user_path(745) # Replace with the actual user ID
       expect(response).to have_http_status(:success)
     end
 
     it "renders the 'show' template" do
       get user_path(745) # Replace with the actual user ID
-      expect(response).to render_template("show")
+      expect(response).to render_template('show')
     end
   end
 end
