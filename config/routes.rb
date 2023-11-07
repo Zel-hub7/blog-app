69f32fa46b5b53c2ac56b691fc7537be7a90e1af
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-07 10:18:28 +0300

    fix routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 end
