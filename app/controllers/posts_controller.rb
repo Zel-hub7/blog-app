ea4ef331d0f8d04dd7379925fbf1607871138544
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-08 20:51:18 +0300

    add view templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
-  def index; end
+  before_action :set_user
 
-  def show; end
+  def index
+    @posts = @user.posts
+  end
+
+  def show
+    # Show the details of an individual post (if needed)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
