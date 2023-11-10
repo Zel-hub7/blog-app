ee8f663a725116f39f7cb492d8001c6a97ce729d
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-10 22:11:23 +0300

    add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+# app/controllers/posts_controller.rb
+
 class PostsController < ApplicationController
   before_action :set_user
 
@@ -11,9 +13,31 @@ class PostsController < ApplicationController
     @likes_count = @post.likes_counter
   end
 
+  def new
+    @post = @user.posts.build
+  end
+
+# app/controllers/posts_controller.rb
+
+  def create
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      flash.now[:error] = @post.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
+# app/models/user.rb
+
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
+
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+
+  after_save :update_posts_counter
 
   def user_recent_posts
     posts.order('created_at DESC').limit(3)
+  end
+
+  private
+
+  def update_posts_counter
+    update(posts_counter: posts&.count.to_i)
   end
 end
