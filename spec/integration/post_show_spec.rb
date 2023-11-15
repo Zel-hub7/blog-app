0000f6c13808278f5e0be1ee8d11fab3ed766b94
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-15 16:13:41 +0300

    add test files

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -52,4 +52,29 @@ RSpec.describe 'Post Details', type: :feature do
     # Your expectations for displaying the comment form
     expect(page).to have_selector("form[action='#{user_post_comments_path(user, post)}'][method='post']")
   end
+
+  scenario 'displays the author of the post' do
+    # Assuming you have a user and a post set up in your database
+    user = User.create(name: 'John Doe', photo: 'user_photo.jpg', bio: 'Some bio', posts_counter: 2)
+    post = Post.create(author: user, title: 'Post Title', text: 'Post content', comments_counter: 0, likes_counter: 0)
+
+    visit user_post_path(user, post)
+
+    # Your expectation for displaying the author's name
+    expect(page).to have_content("Post ##{post.id} by #{user.name}")
+  end
+
+  scenario 'displays the usernames of commenters' do
+    # Assuming you have a user, a post, and some comments set up in your database
+    user = User.create(name: 'John Doe', photo: 'user_photo.jpg', bio: 'Some bio', posts_counter: 2)
+    post = Post.create(author: user, title: 'Post Title', text: 'Post content', comments_counter: 2, likes_counter: 0)
+    comment1 = Comment.create(user: User.create(name: 'Commenter1'), post: post, text: 'First comment')
+    comment2 = Comment.create(user: User.create(name: 'Commenter2'), post: post, text: 'Second comment')
+
+    visit user_post_path(user, post)
+
+    # Your expectations for displaying usernames of commenters
+    expect(page).to have_content(comment1.user.name)
+    expect(page).to have_content(comment2.user.name)
+  end
 end
