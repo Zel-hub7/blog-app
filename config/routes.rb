ee52a3221c1d30777567d9ad2e7ae5870fa5c77f
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-17 17:38:18 +0300

    add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  root 'users#index'
+  root 'home#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
