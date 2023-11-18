cc05fea220f724a9de147fb572f87096d6dbc4a5
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-18 13:50:09 +0300

    add files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
