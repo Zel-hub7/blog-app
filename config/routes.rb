80a92f6a3b8f78c05b2468fd9c4db4ecd74d83e9
Zel-hub7 <Zelalemyohannes75@gmail.com>
2023-11-18 16:34:23 +0300

    add comfirmable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   get 'home/index'
   root 'home#index'
   resources :users, only: [:index, :show] do
