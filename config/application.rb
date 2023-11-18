6bf0312652cba639c533ccc1dc4366b47c80f639
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-18 11:31:22 +0200

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    # Add all subdirectories of `lib` to the autoload paths, except for `assets` and `tasks`.
+    Dir[Rails.root.join('lib', '**/')].each do |directory|
+      config.autoload_paths << directory unless %w(assets tasks).include?(File.basename(directory))
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
